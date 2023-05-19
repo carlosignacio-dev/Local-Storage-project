348591fc86193138e995147e31d01fcfc795802e
Nash-Aud <92648695+Nash-Aud@users.noreply.github.com>
2023-05-19 11:04:02 -0400

    added eventlisteners

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+//Variables
+const formulario = document.querySelector("#formulario");
+const listaTweets = document.querySelector("#lista-tweets");
+
+let tweets = [];
+
+//EventListener
+eventListeners();
+
+function eventListeners() {
+    formulario.addEventListener("submit", agregarTweet);
+}
+
+//Funciones
+function agregarTweet(e) {
+    e.preventDefault();
+    
+    console.log("Agregando Tweet");
+}
