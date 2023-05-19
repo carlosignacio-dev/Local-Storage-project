d25f6ed5bf2569474feeaee5142d60a92ccb5ce1
Nash-Aud <92648695+Nash-Aud@users.noreply.github.com>
2023-05-19 11:15:54 -0400

    added mensaje de error

--- js/app.js
+++ js/app.js
@@ -15,6 +15,31 @@ function eventListeners() {
 //Funciones
 function agregarTweet(e) {
     e.preventDefault();
+
+    //Textarea 
+    const tweet = document.querySelector("#tweet").value;
+
+    //Validacion
+    if(tweet.trim() === "") {
+        mostrarError("Debes ingresar un mensaje");
+        return; //evita que se ejecutan mas lineas de codigo
+    }
     
-    console.log("Agregando Tweet");
+    //Añadir al arreglo de tweets
+}
+
+//Mostrar Mensaje de error
+function mostrarError(error) {
+    const mensajeError = document.createElement("p");
+    mensajeError.textContent = error;
+    mensajeError.classList.add("error");
+
+    //Insertar error en contenido
+    const contenido = document.querySelector("#contenido");
+    contenido.appendChild(mensajeError);
+
+    //Elimina la alerta despues de 3 segundos
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 2000);
 }
